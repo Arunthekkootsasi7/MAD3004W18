9e56e4615a9615add9a7a76029b138ef19a271c8
Arunthekkootsasi7 <35930101+Arunthekkootsasi7@users.noreply.github.com>
2018-02-08 11:39:31 -0500

    Add files via upload

---
+++ day 7/D7 task 2.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+extension Int {
+    var isPrime: Bool {
+        guard self > 1 else {
+            return false
+        }
+        
+        for i in 2..<self {
+            if self % i == 0 {
+                
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+}
+
+
+
+print(7.isPrime)
