2dcc1045fb9f4719ac0e851f5a8f576ab8dc2873
Arunthekkootsasi7 <35930101+Arunthekkootsasi7@users.noreply.github.com>
2018-02-12 12:49:53 -0500

    Add files via upload

---
+++ day_11 access control/day_11 access control/partTime.swift
@@ -0,0 +1,27 @@
+//
+//  partTime.swift
+//  day_11 access control
+//
+//  Created by MacStudent on 2018-02-12.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+
+import Foundation
+
+class PartTime //Student and FullTime cannot be extended as file private
+{
+    var sname: String?
+    fileprivate func setStudentName(sname: String)
+    {
+        self.sname = sname
+    }
+    
+}
+
+internal class ExtendPartTime: PartTime {
+    override internal func setStudentName(sname: String)
+    {
+        super.setStudentName(sname: sname)
+    }
+}
